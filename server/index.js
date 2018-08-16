21d9900ab4a978493f62993dcd8b086d5c569746
housker <housker@gmail.com>
2018-08-16 00:37:02 -0400

    static

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+app.listen(process.env.PORT !== undefined ? process.env.PORT : PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
